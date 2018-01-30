f3d12b8f4d6f03e49bf98d2e1f2dca5f311f7d90
CHENBINGTAI <cbt@store.com>
2018-01-30 15:37:58 +0800

    Initial commit

---
+++ src/com/itheima/mybatis/generatorsqlmap/Generatorsqlmap.java
@@ -0,0 +1,36 @@
+package com.itheima.mybatis.generatorsqlmap;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Generatorsqlmap {
+    public void generator() throws Exception{
+
+        List<String> warnings = new ArrayList<String>();
+        boolean overwrite = true;
+        //指定 逆向工程配置文件
+        File configFile = new File("generator.xml");
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(configFile);
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config,
+                callback, warnings);
+        myBatisGenerator.generate(null);
+    }
+
+    public static void main(String[] args) throws Exception {
+        try {
+            Generatorsqlmap generatorsqlmap = new Generatorsqlmap();
+            generatorsqlmap.generator();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
